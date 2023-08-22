166ede4e4fa8963f6191778e35ec446cfafbe850
Arsen Hryhoriak <136361705+Lifuss@users.noreply.github.com>
2023-08-22 17:34:26 +0300

    github action push

--- src/cat-api.js
+++ src/cat-api.js
@@ -9,18 +9,3 @@ export function fetchBreeds() {
   const catArr = axios.get(`${BASE_URL}breeds`).then(response => response.data);
   return catArr;
 }
-
-// export function fetchCatByBreed(e) {
-//     const searchParam = new URLSearchParams({
-//         breed_ids: e.target.value
-//     })
-//     const params = {
-//         "x-api-key":"live_FMlgfW8OYbvjcjUsCEvuAvTZtUx6LHeUmDdpkyi8NJNmhb0aFyzh0PHlDZbaDWuA"
-//     }
-//     const catArticle = fetch(`https://api.thecatapi.com/v1/images/search?${searchParam}`, params)
-//     .then(data => data.json()).then(renderImg)
-//  }
-
-// function renderImg(arr){
-//     div.innerHTML = `<img src="${arr[0].url}" alt="cat"/>`
-// }
